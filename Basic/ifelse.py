d9b6ce5bed6b5c3432e65043db3139d35f19cbcb
Phantasia0 <ncprog1@gmail.com>
2023-02-07 09:55:03 +0900

    Python Basic

---
+++ Basic/ifelse.py
@@ -0,0 +1,44 @@
+"""
+조건문 if(분기, 중첩)
+"""
+
+# x = 7
+# if x == 7:
+#     print("Lucky")
+#     print("zz")
+
+# x = 15
+# if x >= 10:
+#     if x % 2 == 1:
+#         print("10이상의 홀수")
+
+# x = 7
+# if x > 0 and x < 10:
+#     print("10보다 작은 자연수")
+
+# x = 7
+# if 0 < x < 10:
+#     print("10보다 작은 자연수")
+
+# x = 10
+# if x > 0:
+#     print("양수")
+# else:
+#     print("음수")
+
+# x = 93
+# if x >= 90:
+#     print("A")
+# elif x >= 80:
+#     print("B")
+# else:
+#     print("C")
+
+"""
+반복문 (for,while)
+"""
+# a = range(1, 11)
+# print(list(a))
+
+# for i in range(10):
+#     print("hello")
---
+++ Basic/variable.py
@@ -0,0 +1,59 @@
+"""변수 기초"""
+
+# 값 바꾸기
+# a, b = 10, 20
+# print(a, b)
+# a, b = b, a
+# print(a, b)
+
+# # 변수 타입
+# a = 12345
+# print(a, type(a))
+# a = 12.123456789123456789
+# print(a, type(a))
+# a = "student"
+# print(a, type(a))
+
+# # 출력방식
+# print("number")
+# a, b, c = 1, 2, 3
+# print(a, b, c)
+# print("number : ", a, b, c)
+# print(a, b, c, sep=", ")
+# print(a, b, c, sep="")
+# print(a, b, c, sep="\n")
+# print(a, end=" ")
+# print(b, end=" ")
+# print(c)
+
+"""
+변수 입력과 연산자
+"""
+# a = input("숫자를 입력하세요 : ")
+# print(a)
+
+# a, b = input("숫자를 입력하세요 : ").split()
+# print(type(a))
+# c = a + b
+# print(type(c))
+# print(a, b)
+
+# a, b = input("숫자를 입력하세요 : ").split()
+# a = int(a)
+# b = int(b)
+# print(a, b)
+
+
+# a, b = map(int, input("숫자를 입력하세요 : ").split())
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a**b)
+
+# a = 4.3
+# b = 5
+# c = a + b
+# print(type(c))
