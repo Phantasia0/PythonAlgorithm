d2ebccf8e1fcd2a4e4de5df156b054c461861dd6
Phantasia0 <ncprog1@gmail.com>
2023-02-18 13:14:19 +0900

    SaturDay BFS

--- LessonPart6/8.AA/AA.py
+++ LessonPart6/8.AA/AA.py
@@ -1,3 +1,43 @@
 import sys
+from collections import deque
 
 sys.stdin = open("input.txt", "rt")
+
+n = int(input())
+
+grid = [[0] * n for _ in range(n)]
+check = [[0] * n for _ in range(n)]
+
+for i in range(n):
+    grid[i] = list(map(int, input().split()))
+
+dQ = deque()
+
+dQ.append((n // 2, n // 2))
+check[n // 2][n // 2] = 1
+
+cnt = 0
+sum = grid[n // 2][n // 2]
+L = 0
+while dQ:
+    if L == n // 2:
+        break
+    size = len(dQ)
+
+    for i in range(size):
+        now = dQ.popleft()
+
+        for next in (now[0] - 1, now[0] + 1):
+            if check[next][now[1]] == 0:
+                dQ.append((next, now[1]))
+                sum += grid[next][now[1]]
+                check[next][now[1]] = 1
+        for next in (now[1] - 1, now[1] + 1):
+            if check[now[0]][next] == 0:
+                dQ.append((now[0], next))
+                sum += grid[now[0]][next]
+                check[now[0]][next] = 1
+
+    L += 1
+
+print(sum)
--- LessonPart6/9.AA/AA.py
+++ LessonPart6/9.AA/AA.py
@@ -1,3 +1,49 @@
 import sys
 
 sys.stdin = open("input.txt", "rt")
+from collections import deque
+
+
+grid = [list(map(int, input().split())) for _ in range(7)]
+ch = [[0] * 7 for _ in range(7)]
+
+dx = [0, -1, 0, 1]
+dy = [1, 0, -1, 0]
+
+dQ = deque()
+
+dQ.append((0, 0))
+ch[0][0] = 1
+min = 2222222222222
+cnt = 0
+isFinished = False
+
+while dQ:
+    if isFinished:
+        break
+
+    size = len(dQ)
+
+    for i in range(size):
+        now = dQ.popleft()
+        if now[0] == 6 and now[1] == 6:
+            if cnt < min:
+                min = cnt
+            isFinished = True
+            break
+
+        for j in range(4):
+            x = now[0] + dx[j]
+            y = now[1] + dy[j]
+            if 0 <= x < 7 and 0 <= y < 7:
+                if ch[x][y] == 0:
+                    if grid[x][y] == 0:
+                        dQ.append((x, y))
+                        ch[x][y] = 1
+    cnt += 1
+
+
+if min == 2222222222222:
+    print(-1)
+else:
+    print(min)
